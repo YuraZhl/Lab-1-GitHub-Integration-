a39b4d456363a3c1c2514514d7ca930762c04d12
Owner <Owner@DESKTOP-4LFIRB4>
2017-01-23 21:00:07 -0500

    Initial upload

---
+++ src/lab1github/Lab1GitHub.java
@@ -0,0 +1,17 @@
+
+package lab1github;
+
+/**
+ *
+ * @author Owner
+ */
+public class Lab1GitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World, my GitHub account name is YuraZhl");
+    }
+    
+}
